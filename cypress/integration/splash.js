d551a0f90f4c8ee4a09f62eced2e275a34678d38
Pat Heard <patrick.heard@canada.ca>
2019-09-20 10:07:50 -0400

    Add Cypress visual regression tests
    Uses the cypress-plugin-snapshots plugin (closes #23).

--- cypress/integration/splash.js
+++ cypress/integration/splash.js
@@ -10,6 +10,23 @@ describe('Test the splash page', function() {
         cy.get('title').should('contain', 'Digital Task Force - L’Équipe d’intervention numérique');
     });
 
+    it('matches the reference snapshot on desktop', () => {
+        cy.visit(Cypress.env('host'))
+          .then(() => {
+            cy.document()
+              .toMatchImageSnapshot();
+          });
+    });
+    
+    it('matches the reference snapshot on mobile devices', () => {
+        cy.viewport('iphone-6');
+        cy.visit(Cypress.env('host'))
+          .then(() => {
+            cy.document()
+              .toMatchImageSnapshot();
+          });
+    });    
+
     it('visits the English home page', function() {
         cy.visit(Cypress.env('host'));
         cy.get('main .list-inline a[lang="en"]').click();
